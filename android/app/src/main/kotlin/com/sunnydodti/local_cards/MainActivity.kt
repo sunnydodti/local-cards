73eeaba4d7e9805ea3b61973718b6ab1821d3ce8
Sunny Dodti <sunnydodti@gmail.com>
2025-11-01 20:42:28 +0530

    setup for local auth

--- android/app/src/main/kotlin/com/sunnydodti/local_cards/MainActivity.kt
+++ android/app/src/main/kotlin/com/sunnydodti/local_cards/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.sunnydodti.local_cards
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity ()
